07b2728e00a23cb73d44f879e6f2196e901e74ab
Santiago Delgado Sanchez <151602087+Santi2307@users.noreply.github.com>
2025-08-13 19:16:29 -0400

    fix: vite base + pages action

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// PON aquí EXACTAMENTE el nombre del repo de GitHub
 const repo = "santi-s-portafolio";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: { alias: { "@": "/src" } },
+  plugins: [react()],
+  base: `/${repo}/`,  
 });
 
 
